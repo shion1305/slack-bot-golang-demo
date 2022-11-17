3c0733bcb27fe763d1b207435edb9bbe41c1deaa
Shion Ichikawa <shion1305@gmail.com>
2022-11-17 21:07:58 +0900

    ✅ add main for testing

---
+++ main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"goSlackBotEmpath/pkg/slack"
+	"net/url"
+	"os"
+)
+
+const (
+	EnvClientID     = "clientId"
+	EnvClientSecret = "clientSecret"
+	EnvRedirectURI  = "redirectURI"
+	EnvHost         = "host"
+	EnvPort         = "port"
+	EnvToken        = "token"
+)
+
+func genAuthURI() {
+	//api := slack.NewSlackAPI(os.Getenv("clientId"), os.Getenv("clientSecret"), os.Getenv("redirectURI"))
+	fmt.Println("https://slack.com/oauth/v2/authorize?client_id=" + os.Getenv(EnvClientID) + "&scope=channels:join,channels:manage,chat:write,channels:read,groups:read&redirect_uri=" + os.Getenv(EnvRedirectURI))
+
+}
+
+func main() {
+	godotenv.Load()
+	api := slack.NewSlackAPI(os.Getenv(EnvClientID), os.Getenv(EnvClientSecret), os.Getenv(EnvRedirectURI), os.Getenv(EnvToken))
+
+	client := gin.Default()
+
+	client.GET("/slack/auth", func(c *gin.Context) {
+		authUrl := "https://slack.com/oauth/v2/authorize?client_id=" + os.Getenv("clientId") + "&scope=channels:join,channels:manage,chat:write,channels:read,groups:read&redirect_uri=" + url.QueryEscape(os.Getenv(EnvRedirectURI))
+		fmt.Println(authUrl)
+		c.Redirect(302, authUrl)
+	})
+
+	client.GET("/callback/", func(c *gin.Context) {
+		code := c.Query("code")
+		fmt.Println(code)
+		_, err := api.GetAccessToken(code)
+		if err != nil {
+			c.Writer.WriteString("Error1: " + err.Error() + "\n")
+			return
+		}
+		accessToken, err := api.GetAccessToken(code)
+		if err != nil {
+			c.Writer.WriteString("Error2: " + err.Error() + "\n")
+			return
+		}
+		c.Writer.WriteString("AccessToken: " + accessToken.AccessToken + "\n")
+	})
+	fmt.Println(os.Getenv(EnvHost) + ":" + os.Getenv(EnvPort) + "/slack/auth")
+	client.Run(":" + os.Getenv(EnvPort))
+}
